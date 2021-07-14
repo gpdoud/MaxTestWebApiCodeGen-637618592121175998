92cd3b9dfbc88dadc49e2d3c2fd670e10bd38ae4
Greg Doud <gpdoud@gmail.com>
2021-07-14 11:33:34 -0400

    Committed by GitUtil

---
+++ MaxTestWebApiCodeGen/Models/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MaxTestWebApiCodeGen.Models {
+    public class AppDbContext : DbContext {
+        public DbSet<Customer> Customers { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options) { }
+
+        public AppDbContext() {}
+    }
+}
